398838f29aa55c51ab3be33aefaa6b018de9fec2
wagnerferreiraf <professorwagnerferreira@gmail.com>
2024-10-02 01:10:27 -0400

    updates

---
+++ src/app/contato/page.js
@@ -0,0 +1,10 @@
+const Contato = () => {
+    return (
+        <div>
+        <h1>Contato</h1>
+        <p>Página em construção</p>
+        </div>
+    )
+}
+
+export default Contato;
--- src/app/sobre/page.js
+++ src/app/sobre/page.js
@@ -2,7 +2,7 @@ const Sobre = () => {
     return (
         <div>
             <h1>Sobre</h1>
-            <p>Este é um site de exemplo para o curso de React</p>
+            <p>Página em construção</p>
         </div>
     );
 }
