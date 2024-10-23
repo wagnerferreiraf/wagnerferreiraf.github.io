2f0bba5a1c9653e334fc2d9db42ce3700b3d8800
wagnerferreiraf <professorwagnerferreira@gmail.com>
2024-10-23 14:50:12 -0400

    updates

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,39 +1,35 @@
-import styles from './about.module.css';
+// import { staticGenerationAsyncStorage } from 'next/dist/client/components/static-generation-async-storage-instance';
+import styles from './About.module.css';
 
 const Sobre = () => {
     return (
         <div>
             <h2 className='subtitulo'>Sobre</h2>
-            
-            <div class={styles.containerPrincipal}>
+
+            <div className={styles.containerPrincipal}>
                 <div>
-                    <div class="card-fundo-colorido">
-                        <h2>Atuação profissional</h2>
-                        <div class="card-box-sobre">
-                            <p>Professor efetivo na área da computação do IFRO-Instituto Federal de Rondônia/<em>Campus</em>
-                                Vilhena
-                            </p>
-                            <a class="links" href="https://portal.ifro.edu.br/vilhena" target="_blank"
-                                rel="external">IFRO
-                                Campus
-                                Vilhena</a>
-                        </div>
-                        <div id="frame">
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62272.64224901916!2d-60.1587927021643!3d-12.710803109935942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93b96be3e6498bd7%3A0x66ce4f62185d43bb!2sInstituto%20Federal%20de%20Rond%C3%B4nia%20-%20Campus%20Vilhena!5e0!3m2!1spt-BR!2sbr!4v1707506417767!5m2!1spt-BR!2sbr"
-                                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <h2>Atuação profissional</h2>
+                    <div>
+                        <p>Professor efetivo na área da computação do IFRO-Instituto Federal de Rondônia, <em>Campus</em> Vilhena
+                        </p>
+                        <div className={styles.linkSpace}>
+                            <a className={styles.links} href="https://portal.ifro.edu.br/vilhena" target="_blank" rel="noopener noreferrer">IFRO Campus Vilhena</a>
                         </div>
                     </div>
-
+                    <div id={styles.frame}>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62272.64224901916!2d-60.1587927021643!3d-12.710803109935942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93b96be3e6498bd7%3A0x66ce4f62185d43bb!2sInstituto%20Federal%20de%20Rond%C3%B4nia%20-%20Campus%20Vilhena!5e0!3m2!1spt-BR!2sbr!4v1707506417767!5m2!1spt-BR!2sbr"
+                            allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </div>
 
-                <div class="divs" id="ultimo-container">
-                    <div class="card-fundo-colorido">
+                <div>
+                    <div>
                         <h2>Currículo</h2>
                         <div class="card-box-sobre">
                             <p>Disponível na plataforma Lattes</p>
-                            <a class="links" href="https://lattes.cnpq.br/8387840879676874"
-                                target="_blank">LATTES</a>
+                            <a className={styles.links} class="links" href="https://lattes.cnpq.br/8387840879676874"
+                                target="_blank" rel="noopener noreferrer">LATTES</a>
                         </div>
                     </div>
                 </div>
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,68 +1,36 @@
-import styles from '../about/about.module.css';
+import styles from '../about/About.module.css';
 export default function Contato() {
 
     return (
         <div>
             <h2 className='subtitulo'>Contato</h2>
 
-            <div class="sidebar" id="sidebar">
+            <div class={styles.containerPrincipal}>
+
+                <h2>Redes sociais</h2>
                 <nav>
-                    <ul class="menu">
-                        <li class="item-menu-topo"><a class="link-menu" href="index.html">Home</a></li>
-                        <li class="item-menu-topo"><a class="link-menu" href="sobre.html">Sobre Mim</a></li>
-                        <li class="item-menu-topo"><a class="link-menu" href="contato.html"> Contato</a></li>
-                        <li class="item-menu-topo subitem-toggle"><a class="link-menu" href="#">Slides</a>
-                            <ul>
-                                <li class="item-menu-topo subitem-list"><a class="link-menu" href="slides-front-end-i.html">Front-end I</a></li>
-                                <li class="item-menu-topo subitem-list"><a class="link-menu" href="slides-front-end-ii.html">Front-end II</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+                    <a href="https://www.facebook.com/wagner.ferreira.filho">
+                        <img class={styles.socialImg} src="images/facebook.png" alt="Logo Facebook" />
+                    </a>
+                    <a href="https://www.instagram.com/wagner.fferreira">
+                        <img class={styles.socialImg} src="images/instagram.png" alt="Instagram" />
+                    </a>
+                    <a href="https://www.twitter.com">
+                        <img class={styles.socialImg} src="images/twitter.png" alt="Twitter" />
+                    </a>
                 </nav>
             </div>
+            <div class={`${styles.containerPrincipal} ${styles.lastContainer} `}>
+                <h2>Outros</h2>
+                <div class={styles.containerContacts}>
 
-            <button class="toggle-btn" onclick="toggleSidebar()">☰</button>
-
-            <div class="conteudo">
-                <header>
-                    <div class="primeiro-container">
-                        <h1>Contato</h1>
-                    </div>
-                </header>
-
-                <main>
-                    <div class="divs">
-                        <div class="card-fundo-colorido">
-                            <h2>Redes sociais</h2>
-                            <nav>
-                                <a href="https://www.facebook.com/wagner.ferreira.filho">
-                                    <img class={styles.socialImg} src="images/facebook.png" alt="Logo Facebook" />
-                                </a>
-                                <a href="https://www.instagram.com/wagner.fferreira">
-                                    <img class="rede-social" src="img/instagram.png" alt="Instagram" />
-                                </a>
-                                <a href="https://www.twitter.com">
-                                    <img class="rede-social" src="img/twitter.png" alt="Twitter" />
-                                </a>
-                            </nav>
-                        </div>
-                    </div>
-                    <div class="divs contato" id="ultimo-container">
-                        <div class="card-fundo-colorido">
-                            <h2>Outros</h2>
-                            <a href="mailto:wagner.ferreira@ifro.edu.br">
-                                <img class="rede-social" src="img/mail.png" alt="Contato e-mail" />
-                            </a>
-                            <a href="tel:+556921010700">
-                                <img class="rede-social" src="img/phone.png" alt="Contato telefone" />
-                            </a>
-                        </div>
-                    </div>
-                </main>
-
-                <footer>
-                    <small>&copy;2024 all rights reserved</small>
-                </footer>
+                    <a href="mailto:wagner.ferreira@ifro.edu.br">
+                        <img class={styles.socialImg} src="images/mail.png" alt="Contato e-mail" />
+                    </a>
+                    <a href="tel:+556921010700">
+                        <img class={styles.socialImg} src="images/phone.png" alt="Contato telefone" />
+                    </a>
+                </div>
             </div>
         </div>
     )
